89a4f2213f49cc1629aafa878c6e541a839908ae
Ruitong1997 <fanruitong21@gmail.com>
2023-03-17 20:46:31 +1100

    2023/3/17
    Depth Control Trial

--- passData-7.py
+++ passData-7.py
@@ -1,21 +1,24 @@
 # Passing data from Python to Arduino
 import serial
 import time
-import keyboard
+
 
 arduinoData = serial.Serial('com3', 115200)
     # Cmd: 0->Right 1->Back 2->Forward
     #      3->Left  4->Up   5->Down
     #      6->Forward + Right 7->Forward + Left 8->Back + Right
     #      9->Back + Left     10->Stop
+upNum = 5
+downNum = 50
 while True:
 # myCmd = input('cmd = ')
-    for i in range (30):
-        time.sleep(0.9)
-        # myCmd = input("cmd=")
-        myCmd = str(3)
+    for i in range (downNum):
+        # time.sleep(0.9)
+        myCmd = input("cmd=")
+        # myCmd = str(5)
         myCmd = myCmd + '\r'
         arduinoData.write(myCmd.encode())
+
 
 
     # while keyboard.is_pressed('s'):
